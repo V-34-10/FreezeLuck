75d678cc71c551f37ca90168f8ac4aee0a510b44
Vlad <vladandryschenko98@gmail.com>
2024-02-21 15:18:27 +0200

    Add dependency, landscape portrait, general-activity

---
+++ app/src/main/java/com/forrtun/frreezy/game/ui/privacy/PrivacyActivity.kt
@@ -0,0 +1,12 @@
+package com.forrtun.frreezy.game.ui.privacy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.forrtun.frreezy.game.R
+
+class PrivacyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_privacy)
+    }
+}
--- app/src/main/java/com/forrtun/frreezy/game/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/forrtun/frreezy/game/ui/settings/SettingsActivity.kt
@@ -1,11 +1,12 @@
-package com.forrtun.frreezy.game
+package com.forrtun.frreezy.game.ui.settings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.forrtun.frreezy.game.R
 
-class MainActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_settings)
     }
 }
