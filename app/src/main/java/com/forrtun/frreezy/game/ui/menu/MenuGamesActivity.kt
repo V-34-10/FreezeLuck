ae241d0c6f9bc4d0d2f942b615b61f013fbb5ec3
Vlad <vladandryschenko98@gmail.com>
2024-02-21 21:19:46 +0200

    MenuGamesActivity: UI done

--- app/src/main/java/com/forrtun/frreezy/game/ui/menu/MenuGamesActivity.kt
+++ app/src/main/java/com/forrtun/frreezy/game/ui/menu/MenuGamesActivity.kt
@@ -3,10 +3,16 @@ package com.forrtun.frreezy.game.ui.menu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.forrtun.frreezy.game.R
+import com.forrtun.frreezy.game.databinding.ActivityMainBinding
+import com.forrtun.frreezy.game.databinding.ActivityMenuGamesBinding
+import com.forrtun.frreezy.game.utils.FullScreen
 
 class MenuGamesActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMenuGamesBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu_games)
+        setContentView(binding.root)
+        FullScreen.setFullScreen(this)
     }
 }
