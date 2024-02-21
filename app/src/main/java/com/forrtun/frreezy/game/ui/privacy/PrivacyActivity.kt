fece448bc81727e38de794abcc3a969d601cd623
Vlad <vladandryschenko98@gmail.com>
2024-02-21 20:04:49 +0200

    PrivacyActivity: UI done, add button animation, logic link intentBrowser

--- app/src/main/java/com/forrtun/frreezy/game/ui/privacy/PrivacyActivity.kt
+++ app/src/main/java/com/forrtun/frreezy/game/ui/privacy/PrivacyActivity.kt
@@ -1,12 +1,53 @@
 package com.forrtun.frreezy.game.ui.privacy
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
 import com.forrtun.frreezy.game.R
+import com.forrtun.frreezy.game.databinding.ActivityPrivacyBinding
+import com.forrtun.frreezy.game.ui.menu.MenuActivity
+import com.forrtun.frreezy.game.utils.FullScreen.setFullScreen
 
 class PrivacyActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityPrivacyBinding.inflate(layoutInflater) }
+    private var linkPrivacyPolicy: String = "https://www.google.com" //TODO change on release
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_privacy)
+        setContentView(binding.root)
+        setFullScreen(this)
+        clickAccept()
+    }
+
+    private fun clickAccept() {
+        val animationClick = AnimationUtils.loadAnimation(this, R.anim.button_animation)
+        val btnPrivacyListener = View.OnClickListener {
+            binding.btnAccept.startAnimation(animationClick)
+            runMenu()
+        }
+        binding.btnAccept.setOnClickListener(btnPrivacyListener)
+        binding.textPrivacy.setOnClickListener {
+            binding.textPrivacy.startAnimation(animationClick)
+            runBrowsePrivacy()
+        }
+    }
+
+    private fun runMenu() {
+        startActivity(Intent(this@PrivacyActivity, MenuActivity::class.java))
+        finish()
+    }
+
+    private fun runBrowsePrivacy() =
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(linkPrivacyPolicy)))
+
+    @Deprecated(
+        "Deprecated in Java",
+        ReplaceWith("super.onBackPressed()", "androidx.appcompat.app.AppCompatActivity")
+    )
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
