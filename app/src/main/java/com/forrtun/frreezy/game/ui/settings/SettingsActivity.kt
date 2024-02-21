75ce60c381cdbf142ff8c462ede402b5f8d40416
Vlad <vladandryschenko98@gmail.com>
2024-02-22 00:27:41 +0200

    SettingsActivity: add logic change soundSeek/musicSeek, add resetScore

--- app/src/main/java/com/forrtun/frreezy/game/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/forrtun/frreezy/game/ui/settings/SettingsActivity.kt
@@ -1,8 +1,15 @@
 package com.forrtun.frreezy.game.ui.settings
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.media.AudioManager
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.SeekBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.forrtun.frreezy.game.R
 import com.forrtun.frreezy.game.databinding.ActivitySettingsBinding
 import com.forrtun.frreezy.game.ui.menu.MenuGamesActivity
 import com.forrtun.frreezy.game.utils.FullScreen.setFullScreen
@@ -10,12 +17,81 @@ import com.forrtun.frreezy.game.utils.FullScreen.setFullScreen
 class SettingsActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivitySettingsBinding.inflate(layoutInflater) }
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setFullScreen(this)
+        buttonsControls()
+        controlSoundBar()
     }
 
+    private fun buttonsControls() {
+        val animationClick = AnimationUtils.loadAnimation(this, R.anim.button_animation)
+        binding.btnRemove.setOnClickListener {
+            it.startAnimation(animationClick)
+            resetScore()
+            Toast.makeText(applicationContext, "Balance reset score", Toast.LENGTH_SHORT).show()
+        }
+        binding.btnBack.setOnClickListener {
+            it.startAnimation(animationClick)
+            onBackPressed()
+        }
+    }
+
+    private fun controlSoundBar() {
+        val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        binding.seekBarSound.max = maxVolume
+        val maxMusic = audioManager.getStreamMaxVolume(AudioManager.STREAM_RING)
+        binding.seekBarMusic.max = maxMusic
+
+        binding.seekBarSound.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            var progressChanged = false
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    progressChanged = true
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                progressChanged = false
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                if (progressChanged) {
+                    val volume = seekBar.progress
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0)
+                }
+            }
+        })
+
+        binding.seekBarMusic.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            var progressChanged = false
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    progressChanged = true
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                progressChanged = false
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                if (progressChanged) {
+                    val volume = seekBar.progress
+                    audioManager.setStreamVolume(AudioManager.STREAM_RING, volume, 0)
+                }
+            }
+        })
+    }
+
+    private fun resetScore() {
+        sharedPreferences = getSharedPreferences("FortuneFreezyPref", MODE_PRIVATE)
+        sharedPreferences.edit().putString("Balance", getString(R.string.default_total_balance))
+            .apply()
+    }
 
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
