b319f3484ebefeb5fd12cfa577ed28708ef8634d
Vlad <vladandryschenko98@gmail.com>
2024-02-29 00:56:43 +0200

    SecondGameSlots: fix UI, getCoefficientWin

--- app/src/main/java/com/forrtun/frreezy/game/view/ui/scene/games/MinerSecondGameFragment.kt
+++ app/src/main/java/com/forrtun/frreezy/game/view/ui/scene/games/MinerSecondGameFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
@@ -16,8 +15,12 @@ import com.forrtun.frreezy.game.view.adapter.RecyclerSlotMinerAdapter
 import com.forrtun.frreezy.game.view.adapter.SlotClickListener
 import com.forrtun.frreezy.game.view.manager.ManagerStatusStake
 import com.forrtun.frreezy.game.view.manager.UpdateStatusStake
+import com.forrtun.frreezy.game.view.manager.UpdateStatusStake.constructor
 import com.forrtun.frreezy.game.view.manager.UpdateStatusStake.convertStringToNumber
+import com.forrtun.frreezy.game.view.manager.UpdateStatusStake.getStatusStake
+import com.forrtun.frreezy.game.view.manager.UpdateStatusStake.isTotalSave
 import com.forrtun.frreezy.game.view.manager.UpdateStatusStake.setStatusStake
+import com.forrtun.frreezy.game.view.manager.UpdateStatusStake.setStatusStakeUI
 
 class MinerSecondGameFragment : Fragment(), SlotClickListener {
 
@@ -25,7 +28,6 @@ class MinerSecondGameFragment : Fragment(), SlotClickListener {
     private var originalSlotMutableList = MutableList(12) { R.drawable.ic_slot_7b }
     private lateinit var slotList: List<Slot>
     private lateinit var slotMinerAdapter: RecyclerSlotMinerAdapter
-    private var animationStart = false
     private var defaultBalance = 10000
     private lateinit var managerStatusStake: ManagerStatusStake
     override fun onCreateView(
@@ -34,8 +36,8 @@ class MinerSecondGameFragment : Fragment(), SlotClickListener {
     ): View {
         binding = FragmentMinerSecondGameBinding.inflate(layoutInflater, container, false)
         managerStatusStake =
-            UpdateStatusStake.constructor(UpdateStatusStake.convertStringToNumber(binding.textBalance.text.toString()))
-        UpdateStatusStake.setStatusStakeUI(binding, managerStatusStake)
+            constructor(convertStringToNumber(binding.textBalance.text.toString()))
+        setStatusStakeUI(binding, managerStatusStake)
         return binding.root
     }
 
@@ -44,10 +46,11 @@ class MinerSecondGameFragment : Fragment(), SlotClickListener {
         super.onViewCreated(view, savedInstanceState)
         setSlotRecycler()
         initControlButton()
+        slotMinerAdapter.setSlotMinerClickListener(this)
         activity?.let { context ->
-            if (UpdateStatusStake.isTotalSave(context)) {
-                val (saveTotal) = UpdateStatusStake.getStatusStake(context)
-                val total = UpdateStatusStake.convertStringToNumber(saveTotal.toString())
+            if (isTotalSave(context)) {
+                val (saveTotal) = getStatusStake(context)
+                val total = convertStringToNumber(saveTotal.toString())
                 binding.textBalance.text = "Total\n$total"
             }
         }
@@ -67,13 +70,13 @@ class MinerSecondGameFragment : Fragment(), SlotClickListener {
             animation = AnimationUtils.loadAnimation(context, R.anim.button_animation)
             it.startAnimation(animation)
             managerStatusStake.minusBet()
-            UpdateStatusStake.setStatusStakeUI(binding, managerStatusStake)
+            setStatusStakeUI(binding, managerStatusStake)
         }
         binding.btnPlus.setOnClickListener {
             animation = AnimationUtils.loadAnimation(context, R.anim.button_animation)
             it.startAnimation(animation)
             managerStatusStake.plusBet()
-            UpdateStatusStake.setStatusStakeUI(binding, managerStatusStake)
+            setStatusStakeUI(binding, managerStatusStake)
         }
         binding.btnBack.setOnClickListener {
             animation = AnimationUtils.loadAnimation(context, R.anim.button_animation)
@@ -91,12 +94,14 @@ class MinerSecondGameFragment : Fragment(), SlotClickListener {
         }
     }
 
-    private fun getWinCoefficient(slotList: List<Slot>): Float {
-        return when (slotList.distinct().size) {
-            2 -> 1.0f
-            3 -> 3.0f
-            4 -> 4.0f
-            5 -> 5.0f
+    private fun getWinCoefficient(slot: Slot): Float {
+        return when (slot.image) {
+            R.drawable.ic_slot_4b -> 4.5f
+            R.drawable.ic_slot_5b -> 0.0f
+            R.drawable.ic_slot_1b -> 1.0f
+            R.drawable.ic_slot_3b -> 2.5f
+            R.drawable.ic_slot_2b -> 3.5f
+            R.drawable.ic_slot_6b -> 0.0f
             else -> 0.0f
         }
     }
@@ -107,19 +112,35 @@ class MinerSecondGameFragment : Fragment(), SlotClickListener {
         var win = convertStringToNumber(binding.textWin.text.toString())
         var balance = convertStringToNumber(binding.textBalance.text.toString())
 
-        if (getWinCoefficient(slotList).toInt() == 0) {
+        if (getWinCoefficient(slot).toInt() == 0) {
             balance -= bid
             if (balance < 0) {
                 balance = 0
             }
             binding.textBalance.text = "Total\n$balance"
         } else {
-            val newSumWin = bid * getWinCoefficient(slotList).toInt()
+            val newSumWin = bid * getWinCoefficient(slot).toInt()
             balance += newSumWin
             binding.textBalance.text = "Total\n$balance"
             win += newSumWin
             binding.textWin.text = "WIN\n$win"
         }
+        /*slotList = slotMinerAdapter.getSlotList()
+        val winCoefficient = getWinCoefficient(slotList)
+
+        if (winCoefficient.toInt() == 0) {
+            balance -= bid
+            if (balance < 0) {
+                balance = 0
+            }
+            binding.textBalance.text = "Total\n$balance"
+        } else {
+            val newSumWin = bid * winCoefficient.toInt()
+            balance += newSumWin
+            binding.textBalance.text = "Total\n$balance"
+            win += newSumWin
+            binding.textWin.text = "WIN\n$win"
+        }*/
         activity?.let { it1 ->
             setStatusStake(
                 it1,
