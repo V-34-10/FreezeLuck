df73e6b30672f4a689ff15d3c0cb6dcee489a61b
Vlad <vladandryschenko98@gmail.com>
2024-03-21 18:07:25 +0200

    SplashActivity: fix UI, add permission okhttp3, add usesCleartextTraffic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,9 +51,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    // Import Glide
-    implementation("com.github.bumptech.glide:glide:4.12.0")
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.14.2")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
-    // Volley
-    implementation("com.android.volley:volley:1.2.1")
+    // Okhttp3
+    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation("com.github.bumptech.glide:okhttp3-integration:4.12.0")
 }
